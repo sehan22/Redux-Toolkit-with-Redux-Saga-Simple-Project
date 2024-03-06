2b0c45a5a645b3937894704ca15644113e94ba16
sehan22 <sehanranaweera2002@gmail.com>
2024-03-06 23:23:44 +0530

    redux saga implemented.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {getCatsFetch} from "./catState";
 
 function App() {
+    const cats = useSelector(state => state.cats.cats);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch((getCatsFetch()))
+    }, [dispatch]);
+    console.log(cats)
+
     return (
         <h1>check</h1>
     );
--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga"
 import {configureStore} from "@reduxjs/toolkit";
+
 import catReducer from './catState';
+import catSaga from "./catSaga";
 
 const saga = createSagaMiddleware();
 const store = configureStore({
     reducer: {
         cats: catReducer
     },
-    middleware: [saga]
-})
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), saga]
+});
+
+saga.run(catSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -23,8 +26,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
