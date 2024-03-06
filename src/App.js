cc37c3781e3bb8304b12a70d702fdeacb3b383e6
sehan22 <sehanranaweera2002@gmail.com>
2024-03-06 23:49:40 +0530

    web application finished.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,36 @@ function App() {
     console.log(cats)
 
     return (
-        <h1>check</h1>
+        <div className="App">
+            <h1>CAT SPECIES GALLERY</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab accusantium amet consectetur, dignissimos
+                ex excepturi ipsam itaque labore magnam maiores maxime rerum voluptate? Animi earum non officiis
+                perspiciatis qui.</p>
+
+            <div className="Gallery">
+                {cats.map(cat =>
+                    <div key={cat.id} className="row">
+
+                        {/*<div className="column column-left">
+                            <img
+                                src={cat}
+                                alt={cat.name}
+                                width="200"
+                                height="200"
+                            />
+                            <h1>{cat.cfa_url}</h1>
+                        </div>*/}
+
+                        <div className="column column-right">
+                            <h2>{cat.name}</h2>
+                            <h5>Temperament: {cat.temperament}</h5>
+                            <p>{cat.description}</p>
+                            <a target="_blank" href={cat.wikipedia_url}>Wikipedia</a>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
 
--- src/catSaga.js
+++ src/catSaga.js
@@ -5,7 +5,7 @@ function* workGetCatsFetch() {
     const cats = yield call(() => fetch('https://api.thecatapi.com/v1/breeds'));
     const formattedCats = yield cats.json();
     const formattedCatsShortened = formattedCats.slice(0, 10);
-    yield put(getCatsSuccess(formattedCatsShortened))
+    yield put(getCatsSuccess(formattedCats))
 }
 
 function* catSaga() {
